4248ffcb9686320d180bba78ff648582bed31552
panwang.chengpw <chengpan168@126.com>
2017-06-20 19:11:05 +0800

    cc

--- src/main/java/com/shining3d/saber/spider/Zg3DDayinPage.java
+++ src/main/java/com/shining3d/saber/spider/Zg3DDayinPage.java
@@ -29,7 +29,7 @@ public class Zg3DDayinPage implements PageProcessor {
     private SaberConfig saberConfig;
 
     // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
-    private Site site = Site.me().setRetryTimes(3).setSleepTime(500);
+    private Site site = Site.me().setRetryTimes(5).setSleepTime(5000);
 
     @Override
     public void process(Page page) {
--- src/main/java/com/shining3d/saber/spider/Zg3DDayinPageDetail.java
+++ src/main/java/com/shining3d/saber/spider/Zg3DDayinPageDetail.java
@@ -38,7 +38,7 @@ public class Zg3DDayinPageDetail implements PageProcessor {
     private SaberConfig saberConfig;
 
     // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
-    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+    private Site site = Site.me().setRetryTimes(5).setSleepTime(5000);
 
     @Override
     public void process(Page page) {
